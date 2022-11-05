7c361fb29f31fb6bc76ca909fc66159788e36875
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-11-05 00:32:37 -0400

    need to finish though model

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const { Schema, model}= require('mongoose');
+const { Thought } = require('.');
+
+const UserSchema= new Schema({
+    username: {type: String, required: true, unique: true, trim: true,},
+    email: {type: String, required: true, unique: true,
+            match: [/^([A-Z0-9._%+-]+)@([A-Z0-9.-]+)\.([A-Z]{2,})$/,'Please Enter A Valid Email!'],},
+    thought: [{type: Schema.Types.ObjectId, ref: 'Thought',},],
+    friends: [{type: Schema.Types.ObjectId, ref: 'User',},],},
+    {
+        toJSON: {virtuals: true, getters: true},
+        id: false,
+    });
+
+    const User= model('User', UserSchema);
+    
+    module.exports= User;
